f717e3533d87bf262a086af0c923e04a171337d9
Robson <jrobson.santos81@gmail.com>
2024-05-28 22:15:41 -0300

    first commit

---
+++ cabecalho.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Biblioteca</title>
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Home</a>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Cadastro
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Cadastrar Usuário</a></li>
+                <li><a class="dropdown-item" href="#">Cadastrar Livro</a></li>
+              </ul>
+              
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Administrativo
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Alugar Livro</a></li>
+                <li><a class="dropdown-item" href="#">Devolver Livro</a></li>
+              </ul>
+              
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Relatório
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Listar Livros</a></li>
+                <li><a class="dropdown-item" href="#">Listar Usuários</a></li>
+                <li><a class="dropdown-item" href="#">Relatório de Devolução</a></li>
+              </ul>
+        </div>
+      </div>
+    </nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </body>
+
+</html>
